193fc51ff9254d5a46e2cb5a30013a65d3580a1f
Andreea Ungureanu <xuti@Andreeas-MacBook-Pro.local>
2018-07-24 11:04:50 +0100

    The UI makes some things

--- main/appInterface/MapController.java
+++ main/appInterface/MapController.java
@@ -8,11 +8,7 @@ import com.lynden.gmapsfx.javascript.object.MapOptions;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
-import javafx.stage.Window;
+import javafx.scene.control.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -33,6 +29,8 @@ public class MapController implements Initializable, MapComponentInitializedList
     public void initialize(URL url, ResourceBundle rb) {
         mapView.addMapInializedListener(this);
         findBtn.setDefaultButton(true);
+        // This beautiful line won't allow any other input than integers
+        rangeField.textProperty().addListener(new IntegerOnlyTextListener(rangeField));
     }
 
     @Override
@@ -54,35 +52,70 @@ public class MapController implements Initializable, MapComponentInitializedList
     /* Listener for the search button. */
     @FXML
     protected void searchAction(ActionEvent event) {
-        Window owner = findBtn.getScene().getWindow();
-        if (targetCombo.getItems().isEmpty()) {
-            AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Search error!",
-                    "Please select a unit to search for");
-            return;
-        }
-        System.out.println("Where's my map :(");
+
+        // When adding a range to the field, you have to press the button in order to proceed
+        setRangeField(event);
+        // If there is not selected unit it will pop an alert
+        comboAction(event);
     }
 
     /* Listener for the comboBox button. */
     @FXML
     protected void comboAction(ActionEvent event) {
-        System.out.println("Where's my map :(");
+        if(targetCombo.getValue().equals("")){
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Please select a unit to search for.", ButtonType.CLOSE);
+            alert.setTitle("No unit selected!");
+            alert.showAndWait();
+        }
+        else
+            System.out.println("Where's my map :(");
     }
 
     /* Listener for the comboBox button. */
     @FXML
     protected void setRangeField(ActionEvent event) {
-        Window owner = rangeField.getScene().getWindow();
+        int number1 = parseInt(rangeField.getText(),-1);
 
-        // This beautiful line won't allow any other input than integers
-        rangeField.textProperty().addListener(new IntegerOnlyTextListener(rangeField));
-
-        int number = Integer.parseInt(rangeField.getText());
-
-        if ( number < 0 || number > 50000 )
-            AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Wrong input!",
-                    "Please select a number for the range");
+        if (number1 < 0 || number1 > 50000) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Failed to parse values in the range field.\n" +
+                    "Please ensure the values are in the range.", ButtonType.CLOSE);
+            alert.setTitle("Wrong input!");
+            alert.showAndWait();
+        }
+        else if (number1 == 0) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Failed to parse values in the range field.\n" +
+                    "Please ensure the values are not null.", ButtonType.CLOSE);
+            alert.setTitle("Failed to parse values");
+            alert.showAndWait();
+        }
+        else if (number1 == -1) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Failed to parse values in the range field.\n" +
+                    "Please ensure the values are not null.", ButtonType.CLOSE);
+            alert.setTitle("Failed to parse values");
+            alert.showAndWait();
+        }
         else
-            System.out.println("Where's my map :(");
+            System.out.println("map :(");
+
+
+    }
+
+    /**
+     * Method that catches the null input or 0 input
+     *
+     * @param s the string converted to number
+     * @param valueIfInvalid the returned value if the input is invalid
+     * @return the numeric value for the input
+     */
+    public static int parseInt(final /*@Nullable*/ String s, final int valueIfInvalid) {
+        try {
+            if (s.equals("")) {
+                return valueIfInvalid;
+            } else {
+                return Integer.parseInt(s);
+            }
+        } catch (final NumberFormatException ex) {
+            return valueIfInvalid;
+        }
     }
 }
