09072a689c9639575b7f1a8e86a936aae2f6e309
Basil Mustafa <BasilAMustafa@gmail.com>
2018-07-24 18:42:44 +0100

    make icon hosted online

--- main/appInterface/MapApplication.java
+++ main/appInterface/MapApplication.java
@@ -1,6 +1,7 @@
 package main.appInterface;
 
 import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -8,6 +9,10 @@ import javafx.stage.Stage;
 import javafx.scene.image.Image;
 import main.sqlUtils.Connector;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.net.URL;
 import java.awt.*;
 
 public class MapApplication extends Application {
@@ -27,7 +32,12 @@ public class MapApplication extends Application {
         new Connector();
         Parent root = FXMLLoader.load(getClass().getResource("map_view.fxml"));
 
-        Image icon = new Image(getClass().getResourceAsStream("icon.png"));
+//        Image icon = new Image(getClass().getResourceAsStream("icon.png"));
+//        Image icon = null;
+        URL url = new URL("https://i.imgur.com/XJRK3cG.png)");
+        BufferedImage c= ImageIO.read(url);
+        Image icon = SwingFXUtils.toFXImage(c, null);
+
         stage.getIcons().add(icon);
         stage.setTitle("BBA Innovation Challenge Application");
         stage.setScene(new Scene(root, 800, 500));
