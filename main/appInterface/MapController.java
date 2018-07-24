5e18c3a9bd4b2f0e23a6f4ef928b365318e1c5c2
Basil Mustafa <BasilAMustafa@gmail.com>
2018-07-24 14:41:13 +0100

    add functions for drawing hospital/school markers, displaying radius/resultsets as well as basical examples of usage

--- main/appInterface/MapController.java
+++ main/appInterface/MapController.java
@@ -2,16 +2,22 @@ package main.appInterface;
 
 import com.lynden.gmapsfx.GoogleMapView;
 import com.lynden.gmapsfx.MapComponentInitializedListener;
-import com.lynden.gmapsfx.javascript.object.GoogleMap;
-import com.lynden.gmapsfx.javascript.object.LatLong;
-import com.lynden.gmapsfx.javascript.object.MapOptions;
+import com.lynden.gmapsfx.javascript.object.*;
+import com.lynden.gmapsfx.shapes.Circle;
+import com.lynden.gmapsfx.shapes.CircleOptions;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import main.sqlUtils.FindNearestHospitalRequest;
 import main.sqlUtils.NationalCoverageRequest;
+import main.sqlUtils.SchoolsInRadiusRequest;
 
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class MapController implements Initializable, MapComponentInitializedListener {
@@ -24,6 +30,9 @@ public class MapController implements Initializable, MapComponentInitializedList
     @FXML
     private GoogleMapView mapView;
 
+//    private static LatLong bogota = new LatLong(4.657865, -74.100264);
+    private List<Marker> schoolMarkers = new ArrayList<>();
+    private List<Marker> hospitalMarkers = new ArrayList<>();
     private GoogleMap map;
 
     @Override
@@ -38,7 +47,7 @@ public class MapController implements Initializable, MapComponentInitializedList
     public void mapInitialized() {
         //Set the initial properties of the map.
         MapOptions mapOptions = new MapOptions();
-        mapOptions.center(new LatLong(47.6097, -122.3331))
+        mapOptions.center(new LatLong(4.657865, -74.100264))
                 .overviewMapControl(false)
                 .panControl(false)
                 .rotateControl(false)
@@ -60,6 +69,25 @@ public class MapController implements Initializable, MapComponentInitializedList
         comboAction(event);
 
         new NationalCoverageRequest(map);
+
+        double radius = 2000;
+        //Example display of result sets
+        FindNearestHospitalRequest request = new FindNearestHospitalRequest(map.getCenter());
+
+
+        //Example displaying of result set
+        displayResultSet(request.getRequestResult(), true, true);
+        //Example draw radius
+        drawRadius(map.getCenter(), radius);
+        request.closeRequest();
+
+        //Add schools in the radius
+        SchoolsInRadiusRequest schoolRequest = new SchoolsInRadiusRequest(map.getCenter(), radius);
+        //Do not recentre the map - keep it centred at the hospital
+        displayResultSet(schoolRequest.getRequestResult(), false, false);
+        schoolRequest.closeRequest();
+
+
     }
 
     /* Listener for the comboBox button. */
@@ -99,8 +127,6 @@ public class MapController implements Initializable, MapComponentInitializedList
         }
         else
             System.out.println("map :(");
-
-
     }
 
     /**
@@ -119,6 +145,85 @@ public class MapController implements Initializable, MapComponentInitializedList
             }
         } catch (final NumberFormatException ex) {
             return valueIfInvalid;
+        }
+    }
+
+    /**
+     * Creates a school marker at desired latlong
+     * @param latlong: Coordinates of marker
+     * @param name: Name of marker
+     */
+    protected void schoolMarker(LatLong latlong, String name){
+        //TODO(Basil): Make school/hospital markers look different using MarkerOptions.Icon
+        //TODO(Basil): Include some kind of animation/user interaction using MarkerOptions.Animation
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latlong)
+                .visible(true)
+                .title(name);
+        schoolMarkers.add(new Marker(markerOptions));
+        map.addMarker(schoolMarkers.get(schoolMarkers.size() - 1));
+    }
+
+    /**
+     * Creates a hospital marker at desired LatLong
+     * @param latlong
+     * @param name
+     */
+    protected void hospitalMarker(LatLong latlong, String name){
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latlong)
+                .visible(true)
+                .title(name);
+        Marker marker = new Marker(markerOptions);
+//        hospitalMarkers.add(new Marker(markerOptions));
+//        map.addMarker(hospitalMarkers.get(hospitalMarkers.size() - 1));
+        map.addMarker(marker);
+    }
+
+    /**
+     * Draws radius centred at latlong
+     * @param latlong
+     * @param radius
+     */
+    protected void drawRadius(LatLong latlong, double radius){
+        CircleOptions circleOptions = new CircleOptions();
+        circleOptions.center(latlong)
+                .radius(radius)
+                .strokeColor("blue")
+                .strokeWeight(1)
+                .fillColor("blue")
+                .fillOpacity(0.2);
+
+        Circle c = new Circle(circleOptions);
+
+        map.addMapShape(c);
+    }
+
+    protected void displayResultSet(ResultSet res, Boolean hospital, Boolean recentre){
+        double avgLong = 0;
+        double avgLat = 0;
+        try {
+            res.beforeFirst();
+            while (res.next()) {
+                LatLong coords = new LatLong(res.getDouble(3), res.getDouble(4));
+
+                //If recentering, compute average lat/long for new centre
+                if(recentre){
+                    avgLat += coords.getLatitude();
+                    avgLong += coords.getLongitude();
+                }
+                if (hospital) {
+                    hospitalMarker(coords, res.getString(2));
+                } else {
+                    schoolMarker(coords, res.getString(2));
+                }
+            }
+            if(recentre){
+                int rowCount = res.getRow() + 1;
+                map.setCenter(new LatLong(avgLat/rowCount, avgLong/rowCount));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
