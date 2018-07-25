637c49b75c3f4f889eaa02e48b1e63f9f62ee549
Morgane <morgane.rouvroy@grenoble-inp.org>
2018-07-26 00:37:41 +0200

    BIG improvement in calculation time for coverage map :)

--- main/sqlUtils/NationalCoverageRequest.java
+++ main/sqlUtils/NationalCoverageRequest.java
@@ -3,8 +3,8 @@ package main.sqlUtils;
 public class NationalCoverageRequest extends SQLRequest{
 
     public NationalCoverageRequest(String name) {
-        super("SELECT ST_X((ST_DumpPoints(geom)).geom), " +
-                "ST_Y((ST_DumpPoints(geom)).geom), score FROM departments " +
+        super("SELECT ST_X((ST_DumpPoints(St_SIMPLIFY(geom, 0.005))).geom), " +
+                "ST_Y((ST_DumpPoints(St_SIMPLIFY(geom, 0.005))).geom), score FROM departments " +
                 "WHERE admin1name = ?;",
                 new String[]{name});
     }
