da4c1d22a95ebe3672a566a21859fe4f82f5a56e
Basil Mustafa <BasilAMustafa@gmail.com>
2018-07-27 10:47:04 +0100

    add IsInColombia test

--- test/TestSQL.java
+++ test/TestSQL.java
@@ -2,6 +2,7 @@ package test;
 
 import main.sqlUtils.Connector;
 import main.sqlUtils.FindNearestHospitalRequest;
+import main.sqlUtils.IsInColombiaRequest;
 import main.sqlUtils.SchoolsInRadiusRequest;
 import org.junit.Test;
 
@@ -90,6 +91,56 @@ public class TestSQL {
             String name2 = res2.getString(2);
             assertEquals("Milton Road Primary school", name2);
             request2.closeRequest();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        Connector.closeCon();
+    }
+
+
+    @Test
+    /*
+      Test IsInColumbiaRequest
+
+     */
+    public void testIsInColumbia() {
+        new Connector("jdbc:postgresql://arm-challenge.ckbmr2i1ii7b.eu-west-2.rds.amazonaws.com/Testing",
+                "guest",
+                "guest");
+        assertNotNull(Connector.getCon());
+
+        try {
+            //Variable to hold results
+            ResultSet res;
+
+            //Bogota - should be within
+            IsInColombiaRequest request0 = new IsInColombiaRequest(4.676343, -74.087217);
+            res = request0.getRequestResult();
+            res.next();
+            assertTrue(res.getBoolean(1));
+            request0.closeRequest();
+
+            //Puerto Carreño, just inside of Venezuelan border - should be inside
+            IsInColombiaRequest request1 = new IsInColombiaRequest(6.185448, -67.478935);
+            res = request1.getRequestResult();
+            res.next();
+            assertTrue(res.getBoolean(1));
+            request1.closeRequest();
+
+            //Puerto Páez, just outside Venezuelan border - should be outside
+            IsInColombiaRequest request2 = new IsInColombiaRequest(6.214964, -67.449682);
+            res = request2.getRequestResult();
+            res.next();
+            assertFalse(res.getBoolean(1));
+            request1.closeRequest();
+
+            //Rio de Janeiro in Brazil
+            IsInColombiaRequest request3 = new IsInColombiaRequest(-22.925874, -43.447431);
+            res = request3.getRequestResult();
+            res.next();
+            assertFalse(res.getBoolean(1));
+            request1.closeRequest();
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
